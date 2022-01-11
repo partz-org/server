0fefb4aceedb05e57f0747f08ea261772754b189
Firmin.Giret <firmin.giret@iziwork.com>
2022-01-11 23:21:09 +0100

    chore: add comment

--- src/plugins/redis.ts
+++ src/plugins/redis.ts
@@ -3,6 +3,8 @@ import fastifyRedis, { FastifyRedisPluginOptions } from "fastify-redis";
 import { exec } from "child_process";
 import { promisify } from "util";
 
+// We need to promisify exec because using it the old way
+// with the callback crashes the server :(
 const execAwait = promisify(exec);
 
 export default fp(async (fastify) => {
